42fc6892cabcf34d9d21031aeb71064f627c27d3
Leeke <Leeke@Leeke-PC>
2012-12-20 15:40:11 +0800

    1st

---
+++ template.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Twitter Bootstrap</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<?php include("include/inc_css.php"); ?>
+</head>
+<body data-spy="scroll" data-target=".bs-docs-sidebar">
+<?php include("include/inc_js.php"); ?>
+</body>
+</html>
